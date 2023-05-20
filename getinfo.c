b79c8c356ee5039b197c680994ecb19051ee1c5e
chersana <chersanauwumugisha1@gmail.com>
2023-05-20 12:52:38 -0700

    done

---
+++ getinfo.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+/**
+ * shell_info - initializes information
+ * @info: address
+ */
+void shell_info(info_t *info)
+{
+    info->arg = NULL;
+    info->argv = NULL;
+    info->path = NULL;
+    info->argc = 0;
+}
+/**
+ * _info - initializes info_t struct
+ * @info: struct address
+ * @v: argument
+ */
+void _info(info_t *info, char **v)
+{
+    int n = 0;
+    info->fname = v[0];
+    if (info->arg)
+    {
+        info->argv = s_string(info->arg, " \t");
+        if (!info->argv)
+        {
+            info->argv = malloc(sizeof(char *) * 2);
+            if (info->argv)
+            {
+                info->argv[0] = string_duplicate(info->arg);
+                info->argv[1] = NULL;
+            }
+        }
+        for (n = 0; info->argv && info->argv[n]; n++)
+            ;
+        info->argc = n;
+        _repalias(info);
+        _repvars(info);
+    }
+}
+/**
+ * rel_info - frees info_t struct fields
+ * @info: address
+ * @a: true if freeing all fields
+ */
+void rel_info(info_t *info, int a)
+{
+    free_str(info->argv);
+    info->argv = NULL;
+    info->path = NULL;
+    if (a)
+    {
+        if (!info->cmd_buf)
+            free(info->arg);
+        if (info->env)
+            free_l(&(info->env));
+        if (info->history)
+            free_l(&(info->history));
+        if (info->alias)
+            free_l(&(info->alias));
+        free_str(info->environ);
+            info->environ = NULL;
+        f_pointer((void **)info->cmd_buf);
+        if (info->readfd > 2)
+            close(info->readfd);
+        _putchar(BUF_FLUSH);
+    }
+}
---
+++ history.c
@@ -0,0 +1,129 @@
+#include "shell.h"
+/**
+ * get_hi_file - gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
+char *get_hi_file(info_t *info)
+{
+	char *buf, *dir;
+	dir = ge_to_env(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (string_length(dir) + string_length(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	string_copy(buf, dir);
+	string_concatenate(buf, "/");
+	string_concatenate(buf, HIST_FILE);
+	return (buf);
+}
+/**
+ * write_hi - creates a file or appends to existing file
+ * @info: the parameter struct
+ *
+ * Return: 1 on success, else -1
+ */
+int write_hi(info_t *info)
+{
+	ssize_t fa;
+	char *filename = get_hi_file(info);
+	list_t *node = NULL;
+	if (!filename)
+		return (-1);
+	fa = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fa == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_print_to_fd(node->str, fa);
+		_filedescriptor('\n', fa);
+	}
+	_filedescriptor(BUF_FLUSH, fa);
+	close(fa);
+	return (1);
+}
+/**
+ * read_hi - reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount on success, 0 otherwise
+ */
+int read_hi(info_t *info)
+{
+	int k, last = 0, linecount = 0;
+	ssize_t fa, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_hi_file(info);
+	if (!filename)
+		return (0);
+	fa = open(filename, O_RDONLY);
+	free(filename);
+	if (fa == -1)
+		return (0);
+	if (!fstat(fa, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fa, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fa);
+	for (k = 0; k < fsize; k++)
+		if (buf[k] == '\n')
+		{
+			buf[k] = 0;
+			build_hi_list(info, buf + last, linecount++);
+			last = k + 1;
+		}
+	if (last != k)
+		build_hi_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		del_node_at(&(info->history), 0);
+	renum_history(info);
+	return (info->histcount);
+}
+/**
+ * build_hi_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_hi_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+	if (info->history)
+		node = info->history;
+	add_to_end(&node, buf, linecount);
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+/**
+ * renum_history - recount the history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
+int renum_history(info_t *info)
+{
+	list_t *node = info->history;
+	int k = 0;
+	while (node)
+	{
+		node->num = k++;
+		node = node->next;
+	}
+	return (info->histcount = k);
+}
