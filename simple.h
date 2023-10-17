2b3f5c60b80e34d26dd5aebe56b0f4e4df27b906
nanaoliviane <nanaoliviane@gmail.com>
2023-10-17 13:34:59 -0700

    shell project

--- simple.h
+++ simple.h
@@ -1,6 +1,17 @@
 #ifndef SIMPLE_H
 #define SIMPLE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
 int _putchar(char c);
 
 char *_getenv(info_t *, const char *);
@@ -12,6 +23,10 @@ int populate_env_list(info_t *);
 char **_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
+
+int exit_function(info_t *);
+int cd_function(info_t *);
+int help_function(info_t *);
 
 void execute_command(const char *cmd, char *arguments[]);
 void execute_command_with_args(const char *command_line);
